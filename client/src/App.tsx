9d5020739a946750085a24305a64c5cf782dd88e
hoiJuwon <withjuwon@gmail.com>
2022-11-06 06:26:35 +0900

    feat: add home to root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import {GlobalStyle} from './GlobalStyle'
-import { Header } from './components/Header';
-
+import React from "react";
+import logo from "./logo.svg";
+import { GlobalStyle } from "./GlobalStyle";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header></Header>
+      <Home />
     </>
   );
 }
