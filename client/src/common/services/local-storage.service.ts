a0f8c60cba1520b3a56cd85f0072dd1cb8222246
parkjihwanjay <cc6656@naver.com>
2022-11-06 07:54:18 +0900

    localstorage service 수정

--- client/src/common/services/local-storage.service.ts
+++ client/src/common/services/local-storage.service.ts
@@ -1,19 +1,43 @@
+type LocalStorageKey = "nickName" | "account";
+
+const NICKNAME_KEY = "swallow-nickname";
+const ACCOUNT_KEY = "swallow-account";
 class LocalStorageService {
   constructor() {}
-  get(key: string) {
-    console.log('localStorage get', key);
-    return localStorage.getItem(key);
+  set(key: LocalStorageKey, value: string) {
+    switch (key) {
+      case "nickName":
+        return this.setNickName(value);
+      case "account":
+        return this.setAccount(value);
+    }
   }
-  set(key: string, value: string) {
-    console.log('localStorage set', key, value);
-    return localStorage.setItem(key, value);
+  get(key: LocalStorageKey) {
+    switch (key) {
+      case "nickName":
+        return this.getNickName();
+      case "account":
+        return this.getAccount();
+    }
+  }
+  private setNickName(nickName: string) {
+    return localStorage.setItem(NICKNAME_KEY, nickName);
+  }
+  private getNickName() {
+    return localStorage.getItem(NICKNAME_KEY);
+  }
+  private setAccount(account: string) {
+    return localStorage.setItem(ACCOUNT_KEY, account);
+  }
+  private getAccount() {
+    return localStorage.getItem(ACCOUNT_KEY);
   }
   remove(key: string) {
-    console.log('localStorage remove', key);
+    console.log("localStorage remove", key);
     return localStorage.removeItem(key);
   }
   clear() {
-    console.log('localStorage clear');
+    console.log("localStorage clear");
     return localStorage.clear();
   }
 }
