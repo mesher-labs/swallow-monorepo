ec37eccdd071fc97c7f3aae29c9eaa9fd643ccd4
SeongwonChung <tjddnjs1013@gmail.com>
2022-11-05 18:48:22 -0700

    feat: subgraph config

--- subgraph_swallow/src/shortcuts.ts
+++ subgraph_swallow/src/shortcuts.ts
@@ -1,3 +1,16 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Shortcuts } from "../generated/Shortcuts/Shortcuts"
-import { ExampleEntity } from "../generated/schema"
+import { AddShortCutCall } from "../generated/Shortcuts/Shortcuts";
+import { ShortCut } from "../generated/schema";
+
+export function handleAddShortCut(call: AddShortCutCall): void {
+  const id = call.transaction.hash.toHex();
+  const shortCut = new ShortCut(id);
+  shortCut.name = call.inputs.dto.name;
+  shortCut.endpoint = call.inputs.dto.endpoint;
+  shortCut.selector = call.inputs.dto.selector;
+  shortCut.contractAddr = call.inputs.dto.contractAddr;
+  shortCut.shortCutsType = call.inputs.dto.shortCutsType;
+  // shortCut.userParams = call.inputs.dto.userParams
+  // TODO: add userParams
+
+  shortCut.save();
+}
