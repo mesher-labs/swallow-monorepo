44709842315fdf92603deac7313946fe560453d3
parkjihwanjay <cc6656@naver.com>
2022-11-06 14:26:12 +0900

    add type

--- client/src/components/Home/HomePresenter.tsx
+++ client/src/components/Home/HomePresenter.tsx
@@ -15,21 +15,9 @@ import {
   DefaultMultiSendShortcut,
   DefaultTokenBalanceShortcut,
 } from "./DefaultShortCuts";
+import { ShortcutTypes } from "../../common/types/short-cuts.types";
 
-export type ShortcutTypes =
-  | "SEND"
-  | "BUY"
-  | "AAVE_CURRENT_APY"
-  | "MULTI_SEND"
-  | "TOKEN_BALANCE";
 
-export const Shortcuts = {
-  SEND: "SEND",
-  BUY: "BUY",
-  AAVE_CURRENT_APY: "AAVE_CURRENT_APY",
-  MULTI_SEND: "MULTI_SEND",
-  TOKEN_BALANCE: "TOKEN_BALANCE",
-} as const;
 
 export interface HomePresenterProps {
   shortcutType: ShortcutTypes;
---
+++ client/src/utils/ethers.utils.ts
@@ -0,0 +1,34 @@
+import { ethers } from "ethers";
+import { EventFragment } from "ethers/lib/utils";
+
+const iface =  new ethers.utils.Interface([
+  "function requestGTToMBXLSwap(uint amountIn, uint amountOutMin, address[] calldata path, address to) external"
+]);
+
+
+class EthersUtil {
+
+  static encodeFunctionData(fn: string, params: any[]): string {
+    return iface.encodeFunctionData(fn, params);
+  }
+  static decodeRawTx(rawTx: string) {
+    return ethers.utils.parseTransaction(rawTx);
+  }
+  static decodeFunctionData(functionData: string) {
+    return iface.parseTransaction({ data: functionData });
+  }
+  static decodeFunctionResult(fn: string, resultData: string) {
+    return iface.decodeFunctionResult(fn, resultData);
+  }
+  static encodeFilterTopics(eventFragment: EventFragment, topics: string[]) {
+    return iface.encodeFilterTopics(eventFragment, topics);
+  }
+  static decodeLog(topics: string[], data: string) {
+    return iface.parseLog({
+      data,
+      topics,
+    });
+  }
+}
+
+export default EthersUtil;
