b2f178bc2ef486461ef2c9e89f9f47114c4256ca
SeongwonChung <tjddnjs1013@gmail.com>
2022-11-05 19:08:23 -0700

    feat: subgraph mapping done

--- subgraph_swallow/src/shortcuts.ts
+++ subgraph_swallow/src/shortcuts.ts
@@ -1,16 +1,25 @@
 import { AddShortCutCall } from "../generated/Shortcuts/Shortcuts";
-import { ShortCut } from "../generated/schema";
+import { ShortCut, UserParam } from "../generated/schema";
 
 export function handleAddShortCut(call: AddShortCutCall): void {
-  const id = call.transaction.hash.toHex();
-  const shortCut = new ShortCut(id);
+  const shortCutId = call.transaction.hash.toHex();
+  const shortCut = new ShortCut(shortCutId);
+
+  call.inputs.dto.userParams.forEach(({ name, value }, index) => {
+    const paramId = shortCutId + "-" + index;
+    const userParam = new UserParam(paramId);
+    userParam.name = name;
+    userParam.value = value;
+    userParam.shortcut = shortCutId;
+
+    userParam.save();
+  });
+
   shortCut.name = call.inputs.dto.name;
   shortCut.endpoint = call.inputs.dto.endpoint;
   shortCut.selector = call.inputs.dto.selector;
   shortCut.contractAddr = call.inputs.dto.contractAddr;
   shortCut.shortCutsType = call.inputs.dto.shortCutsType;
-  // shortCut.userParams = call.inputs.dto.userParams
-  // TODO: add userParams
 
   shortCut.save();
 }
