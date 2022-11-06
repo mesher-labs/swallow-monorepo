6d3293d60439a319076811232b010049ca847d73
SeongwonChung <tjddnjs1013@gmail.com>
2022-11-06 02:13:52 -0800

    chore: set test shortcuts

--- contract/scripts/add.ts
+++ contract/scripts/add.ts
@@ -12,31 +12,48 @@ enum ShortcutType {
 const NULL_STRING = "NULL";
 const NULL_ADDRESS = ethers.constants.AddressZero;
 
-const DUMMY = {
-  shortcutType: ShortcutType.BUY,
-  endpoint: "https://mumbai.api.0x.org/",
+const SEND = {
+  shortcutType: ShortcutType.SEND,
+  endpoint: NULL_STRING,
   contractAddr: NULL_ADDRESS,
-  userParams: [
-    { name: "buyToken", value: NULL_STRING },
-    { name: "sellToken", value: NULL_STRING },
-  ],
   isReady: true,
 };
+const BUY = {
+  shortcutType: ShortcutType.BUY,
+  endpoint: "https://mumbai.api.0x.org/swap/v1/quote",
+  contractAddr: NULL_ADDRESS,
+  isReady: true,
+};
+const AAVE = {
+  shortcutType: ShortcutType.AAVE_CURRENT_APY,
+  endpoint: "https://api.thegraph.com/subgraphs/name/messari/aave-v2-polygon-extended",
+  contractAddr: NULL_ADDRESS,
+  isReady: true,
+};
+const MULTI_SEND = {
+  shortcutType: ShortcutType.MULTI_SEND,
+  endpoint: NULL_STRING,
+  contractAddr: NULL_ADDRESS,
+  isReady: false,
+};
+const TOKEN_BALANCE = {
+  shortcutType: ShortcutType.TOKEN_BALANCE,
+  endpoint: NULL_STRING,
+  contractAddr: NULL_ADDRESS,
+  isReady: false,
+};
 
-const main = async () => {
+const main = async (DUMMY: any) => {
   const gas = await connectedContract.estimateGas.addShortcut(
     DUMMY.shortcutType,
     DUMMY.endpoint,
     DUMMY.contractAddr,
-    DUMMY.userParams,
     DUMMY.isReady,
   );
-
   const result = await connectedContract.addShortcut(
     DUMMY.shortcutType,
     DUMMY.endpoint,
     DUMMY.contractAddr,
-    DUMMY.userParams,
     DUMMY.isReady,
     { gasLimit: gas },
   );
@@ -50,5 +67,5 @@ const view = async () => {
   console.log(result);
 };
 
-main();
+// main(TOKEN_BALANCE);
 // view();
