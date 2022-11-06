07eddc60c73d914a9eba68b2c5f546699ff92ef4
everGreenGH <skin0011@naver.com>
2022-11-06 01:31:10 -0700

    feat: add scripts for fetching thegraph

---
+++ client/src/utils/fetchShortcuts.ts
@@ -0,0 +1,28 @@
+import { useQuery, useQueryClient } from "react-query";
+
+fetch("https://api.thegraph.com/subgraphs/name/evergreengh/swallow", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    query: `
+        {
+            shortcuts {
+            id
+            index
+            shortcutType
+            userParams {
+                name
+                value
+            }
+            endpoint
+            isReady
+            }
+        }
+    `,
+    variables: {},
+  }),
+})
+  .then((res) => res.json())
+  .then((result) => console.log(result.data));
